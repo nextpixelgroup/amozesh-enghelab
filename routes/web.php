9596926e70aba8fdd83582769083c6503a32633e
hosseinhezareh <hosseinhezareh2@gmail.com>
2025-10-26 16:34:52 +0330

    coming soon page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+if(env('APP_ENV') === 'production') {
+    Route::domain('amozesh.enghelab.ir')->group(function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    });
+}
 
 
 Route::get('/test', [TestController::class, 'index']);
