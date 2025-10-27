46c3aee310b46d1832a22749a4b310dbfeae0261
hosseinhezareh <hosseinhezareh2@gmail.com>
2025-10-27 18:00:11 +0330

    develop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>به زودی!</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo.png') }}">
     <script src="{{ asset('tailwind.js') }}"></script>
 
     <!-- فونت فارسی Vazir -->
