8112cdcce521125a618fbe399087c81c4f497064
hosseinhezareh <hosseinhezareh2@gmail.com>
2025-10-27 12:04:55 +0330

    develop

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -3,6 +3,17 @@ import createServer from '@inertiajs/vue3/server'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h } from 'vue'
 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: { defaultTheme: 'light' },
+})
+
 createServer(page =>
     createInertiaApp({
         page,
@@ -14,7 +25,9 @@ createServer(page =>
         setup({ App, props, plugin }) {
             return createSSRApp({
                 render: () => h(App, props),
-            }).use(plugin)
+            })
+                .use(plugin)
+                .use(vuetify)
         },
     }),
 )
