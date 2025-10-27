738f28c28292ee1936b71c0475767bce2df21572
hosseinhezareh <hosseinhezareh2@gmail.com>
2025-10-27 17:21:44 +0330

    develop

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,24 +5,21 @@ import { createInertiaApp } from '@inertiajs/vue3'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import { VApp, VMain, VForm, VTextField, VBtn } from 'vuetify/components'
-import { Ripple } from 'vuetify/directives'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
-    components: { VApp, VMain, VForm, VTextField, VBtn },
-    directives: { Ripple },
+    components,
+    directives,
     theme: {
         defaultTheme: 'light',
     },
 })
 
 createInertiaApp({
-    resolve: async name => {
-        const pages = import.meta.glob('./Pages/**/*.vue')
-        const importer = pages[`./Pages/${name}.vue`]
-        if (!importer) throw new Error(`Page not found: ${name}`)
-        const mod = await importer()
-        return mod.default
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
