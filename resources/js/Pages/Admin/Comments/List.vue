415964f8961f242e85978667dd212ca3f8c7c6a3
stevenmaz <steven.m@realtyna.net>
2025-11-03 03:48:58 -0500

    Add Comments

--- resources/js/Pages/Admin/Comments/List.vue
+++ resources/js/Pages/Admin/Comments/List.vue
@@ -51,37 +51,46 @@
             </v-row>
         </v-card>
         <v-card class="pa-3 elevation-2">
-            <v-table density="compact">
+            <v-table>
                 <thead>
                 <tr>
                     <th class="text-center">ردیف</th>
                     <th class="text-center">کاربر</th>
-                    <th class="text-right">عنوان</th>
-                    <th class="text-center">نویسنده</th>
-                    <th class="text-center">وضعیت</th>
-                    <th class="text-center">قیمت</th>
-                    <th class="text-center">موجودی</th>
+                    <th class="text-right">دوره</th>
+                    <th class="text-center">نظر</th>
+                    <th class="text-center">تاریخ ارسال</th>
                     <th class="text-center">عملیات</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr
-                    v-for="item in books"
-                    :key="item.name"
+                    v-for="item in comments"
+                    :key="item.row"
                 >
-                    <td class="text-center">{{ item.row }}</td>
-                    <td class="text-center">
-                        <img :src="item.image" :alt="item.title" width="90" height="90">
+                    <td class="text-center" colspan="1">{{ item.row }}</td>
+                    <td width="200px">
+                        <div class="d-flex align-items-center ga-3">
+                            <v-avatar>
+                                <v-img
+                                    :alt="item.author"
+                                    :src="item.avatar"
+                                ></v-img>
+                            </v-avatar>
+                            <div>
+                                <strong class="d-block">{{ item.author }}</strong>
+                                <small>{{ item.type }}</small>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="text-center" colspan="1">
+                        {{ item.course }}
                     </td>
                     <td>
-                        <strong class="d-block">{{ item.title }}</strong>
-                        <span class="d-lg-block d-sm-none">{{ item.subtitle }}</span>
+                        {{ item.comment }}
                     </td>
-                    <td class="text-center">
-                        {{ item.author }}
-                    </td>
-                    <td class="text-center">
-                        <span :class="item.status">{{ item.status_name }}</span>
+                    <td>
+                        <span class="d-block">{{ item.date }}</span>
+                        <small>{{ item.time }}</small>
                     </td>
                     <td>
                         <div class="d-flex ga-1">
@@ -102,39 +111,95 @@ import {ref} from "vue";
 import AdminLayout from "../../../Layouts/AdminLayout.vue";
 import {route} from "ziggy-js";
 
-const books = ref([
+const comments = ref([
     {
         "id": "1",
         "row": "1",
-        "avatar": '/assets/img/prof/4.jpg',
-        "author": "حسین نوابیان",
-        "type": "موسسه آموزش عالی فردوس",
-        "comment": "سلام خوبین",
-        "course": "دوره مدیریت فرهنگی جبهه انقلاب",
-        "date": "1404/07/19",
-        "time": "9:42 ب.ظ"
+        "avatar": '/assets/img/prof/2.jpg',
+        "author": "حمید بصیرت منش",
+        "type": "دانشگاه امام صادق (ع)",
+        "comment": "سلام وقتتون بخیر. جسارتا من نمیتونم ویدیو شماره 3 دوره را مشاهده کنم. لطف می کنید بگین مشکل چی هست؟",
+        "course": "کتاب جهان پس از ظهور",
+        "date": "1404/10/29",
+        "time": "10:12 ب.ظ"
     },
     {
         "id": "2",
         "row": "2",
-        "avatar": '/assets/img/prof/2.jpg',
-        "author": "حسین نوابیان",
-        "type": "موسسه آموزش عالی فردوس",
-        "comment": "سلام خوبین",
-        "course": "دوره مدیریت فرهنگی جبهه انقلاب",
-        "date": "1404/07/19",
-        "time": "9:42 ب.ظ"
+        "avatar": '/assets/img/prof/3.jpg',
+        "author": "محسن سراج",
+        "type": "دانشگاه صنعتی امیرکبیر",
+        "comment": "دوره بسیار کاربردی و منظم برگزار شد. سپاس از گروه آموزش.",
+        "course": "دوره تحلیل تاریخ معاصر ایران",
+        "date": "1404/09/10",
+        "time": "7:39 ب.ظ"
     },
     {
         "id": "3",
         "row": "3",
-        "avatar": '/assets/img/prof/3.jpg',
-        "author": "حسین نوابیان",
-        "type": "موسسه آموزش عالی فردوس",
-        "comment": "سلام خوبین",
-        "course": "دوره مدیریت فرهنگی جبهه انقلاب",
-        "date": "1404/07/19",
-        "time": "9:42 ب.ظ"
+        "avatar": '/assets/img/prof/4.jpg',
+        "author": "سیدمحمود سادات",
+        "type": "دانشگاه فردوسی مشهد",
+        "comment": "سلام و عرض ادب، لطفاً لینک جلسه‌ی اول را مجدداً در اختیار بگذارید، لینک فعلی باز نمی‌شود.",
+        "course": "دوره تاریخ دفاع مقدس",
+        "date": "1404/06/12",
+        "time": "7:59 ب.ظ"
     },
+    {
+        "id": "4",
+        "row": "4",
+        "avatar": "/assets/img/prof/5.jpg",
+        "author": "فاطمه کرمی",
+        "type": "دانشگاه تهران",
+        "comment": "سلام، فایل جزوه‌ی جلسه‌ی دوم در سامانه بارگذاری نشده. امکان داره بررسی بفرمایید؟",
+        "course": "دوره فلسفه اسلامی",
+        "date": "1404/07/18",
+        "time": "9:45 ق.ظ"
+    },
+    {
+        "id": "5",
+        "row": "5",
+        "avatar": "/assets/img/prof/6.jpg",
+        "author": "علی موسوی‌نیا",
+        "type": "دانشگاه علامه طباطبایی",
+        "comment": "بسیار دوره مفیدی بود، خصوصاً بخش تحلیل موردی‌ها. تشکر از اساتید محترم.",
+        "course": "دوره تحلیل تاریخ معاصر ایران",
+        "date": "1404/08/25",
+        "time": "6:27 ب.ظ"
+    },
+    {
+        "id": "6",
+        "row": "6",
+        "avatar": "/assets/img/prof/7.jpg",
+        "author": "زهرا رضایی",
+        "type": "دانشگاه شهید بهشتی",
+        "comment": "سلام وقت بخیر. آزمون پایان دوره چه زمانی برگزار می‌شود؟",
+        "course": "کتاب موعود در آیینه ادیان",
+        "date": "1404/11/02",
+        "time": "11:10 ق.ظ"
+    },
+    {
+        "id": "7",
+        "row": "7",
+        "avatar": "/assets/img/prof/2.jpg",
+        "author": "محمدصادق کیانی",
+        "type": "دانشگاه اصفهان",
+        "comment": "ممنون از محتوای خوب و استاد گرامی. فقط لطفاً ویدیوها با کیفیت بالاتری قرار بدید.",
+        "course": "دوره تحلیل تاریخ معاصر ایران",
+        "date": "1404/09/30",
+        "time": "8:50 ب.ظ"
+    },
+    {
+        "id": "8",
+        "row": "8",
+        "avatar": "/assets/img/prof/3.jpg",
+        "author": "الهام احمدی",
+        "type": "دانشگاه الزهرا (س)",
+        "comment": "ثبت‌نام من در دوره انجام شده اما هنوز به محتوای دروس دسترسی ندارم.",
+        "course": "دوره تاریخ دفاع مقدس",
+        "date": "1404/10/03",
+        "time": "5:33 ب.ع"
+    }
+
 ])
 </script>
