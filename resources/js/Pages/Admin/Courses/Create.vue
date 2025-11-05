26519dc89e2fb08e3c3fa517dc1c2aeed95802e7
stevenmaz <steven.m@realtyna.net>
2025-11-05 04:22:07 -0500

    Made some adjustments (UI) for the Create Course page.

--- resources/js/Pages/Admin/Courses/Create.vue
+++ resources/js/Pages/Admin/Courses/Create.vue
@@ -16,7 +16,6 @@
                             />
                         </v-col>
                         <v-col class="v-col-12">
-
                             <v-text-field
                                 type="text"
                                 v-model="course.slug"
@@ -103,11 +102,186 @@
                     label="جستجوی پیش نیازها..."
                     empty-message="هیچ پیش‌نیازی انتخاب نشده است."
                 />
-                <v-btn type="button" @click="addSeason">افزودن سرفصل جدید</v-btn>
+                <div class="zo-header-section mb-5">
+                    <v-row class="align-center">
+                        <v-col class="v-col-lg-9">
+                            <div class="zo-info d-lg-flex d-sm-none">
+                                <div class="zo-icon elevation-4">
+                                    <i class="mdi mdi-book-open-page-variant"></i>
+                                </div>
+                                <div class="zo-name">
+                                    <strong class="d-block mb-1">افزودن سرفصل</strong>
+                                    <span>در این بخش می توانید سرفصل های دوره + دروس را ایجاد کنید.</span>
+                                </div>
+                            </div>
+                        </v-col>
+                    </v-row>
+                </div>
+                <v-expansion-panels multiple class="mb-3">
+                    <v-expansion-panel>
+                        <div class="zo-actions">
+                            <div class="zo-switch">
+                                <v-switch v-model="switch_value" color="success"></v-switch>
+                            </div>
+                        </div>
+                        <div class="zo-close">
+                            <v-btn icon="mdi-close" width="25" height="25" color="error"></v-btn>
+                        </div>
+                        <v-expansion-panel-title>
+                            سرفصل 1
+                        </v-expansion-panel-title>
+                        <v-expansion-panel-text>
+                            <v-text-field
+                                hide-details
+                                variant="outlined"
+                                density="comfortable"
+                                label="عنوان سرفصل"
+                                type="text"
+                                class="mb-3"
+                                prepend-inner-icon="mdi-text-short"
+                            />
+                            <v-textarea
+                                hide-details
+                                variant="outlined"
+                                density="comfortable"
+                                type="text"
+                                label="توضیحات سرفصل"
+                                class="mb-3"
+                                prepend-inner-icon="mdi-text"
+                                rows="3"
+                            />
+                            <v-row dense class="justify-end position-relative mb-3">
+                                <v-col class="v-col-12 pe-12">
+                                    <v-expansion-panels multiple>
+                                        <v-expansion-panel>
+                                            <div class="zo-actions">
+                                                <div class="zo-switch">
+                                                    <v-switch v-model="switch_value" color="success"></v-switch>
+                                                </div>
+                                            </div>
+                                            <div class="zo-close">
+                                                <v-btn icon="mdi-close" width="25" height="25" color="error"></v-btn>
+                                            </div>
+                                            <v-expansion-panel-title>
+                                                درس 1
+                                            </v-expansion-panel-title>
+                                            <v-expansion-panel-text>
+                                                <v-card flat>
+                                                    <v-row dense class="position-relative">
+                                                        <v-col class="v-col-lg-9 v-col-12">
+                                                            <v-text-field
+                                                                hide-details
+                                                                variant="outlined"
+                                                                density="comfortable"
+                                                                label="عنوان درس"
+                                                                type="text"
+                                                                prepend-inner-icon="mdi-text-short"
+                                                            />
+                                                        </v-col>
+                                                        <v-col class="v-col-lg-3 v-col-12">
+                                                            <v-text-field
+                                                                hide-details
+                                                                variant="outlined"
+                                                                density="comfortable"
+                                                                label="زمان ویدیو"
+                                                                type="text"
+                                                                prepend-inner-icon="mdi-clock-time-eight-outline"
+                                                            />
+                                                        </v-col>
+                                                        <v-col class="v-col-12">
+                                                            <v-textarea
+                                                                hide-details
+                                                                variant="outlined"
+                                                                density="comfortable"
+                                                                type="text"
+                                                                label="توضیحات درس"
+                                                                rows="2"
+                                                                prepend-inner-icon="mdi-text"
+                                                            />
+                                                        </v-col>
+                                                        <v-col class="v-col-12">
+                                                            <v-text-field
+                                                                type="text"
+                                                                hide-details
+                                                                variant="outlined"
+                                                                density="comfortable"
+                                                                label="لینک ویدیو"
+                                                                suffix="https://video.amozesh.enghelab.test/courses/1/"
+                                                                dir="ltr"
+                                                                prepend-inner-icon="mdi-link"
+                                                            >
+                                                                <template v-slot:append>
+                                                                    <v-btn
+                                                                        icon
+                                                                        variant="text"
+                                                                        @click=""
+                                                                        title="مشاهده"
+                                                                    >
+                                                                        <v-icon>mdi-open-in-new</v-icon>
+                                                                    </v-btn>
+                                                                </template>
+                                                            </v-text-field>
+                                                        </v-col>
+                                                        <v-col class="v-col-12">
+                                                            <v-file-upload
+                                                                density="compact"
+                                                                variant="compact"
+                                                                title="بارگذاری تصویر ویدیو"
+                                                                accept="image/*"
+                                                                label="فقط فایل تصویری آپلود کنید"
+                                                                :rules="[v => !v || v.type.startsWith('image/') || 'فقط فایل تصویری مجاز است']"
+                                                            >
+                                                            </v-file-upload>
+                                                        </v-col>
+                                                    </v-row>
+                                                    <v-row dense>
+                                                        <v-col class="v-col-12">
+                                                            <v-checkbox v-model="checkbox_value"
+                                                                        hide-details
+                                                                        label="فعال سازی آزمون؟"
+                                                            >
+                                                            </v-checkbox>
+                                                        </v-col>
+                                                        <v-col class="v-col-12">
+                                                            <v-textarea
+                                                                hide-details
+                                                                variant="outlined"
+                                                                density="comfortable"
+                                                                type="text"
+                                                                label="توضیحات آزمون"
+                                                                rows="2"
+                                                                prepend-inner-icon="mdi-text"
+                                                            />
+                                                        </v-col>
+                                                    </v-row>
+                                                    <v-row dense>
+                                                    </v-row>
+                                                    <div class="zo-add text-end">
+                                                        <v-btn icon="mdi-plus"
+                                                               width="25"
+                                                               height="25"
+                                                               color="primary"
+                                                        >
+                                                        </v-btn>
+                                                    </div>
+                                                </v-card>
+                                            </v-expansion-panel-text>
+                                        </v-expansion-panel>
+                                    </v-expansion-panels>
+                                </v-col>
+                            </v-row>
+                            <div class="text-left">
+                                <v-btn color="primary">افزودن درس</v-btn>
+                            </div>
+                        </v-expansion-panel-text>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+                <div class="text-left">
+                    <v-btn color="primary">افزودن سرفصل</v-btn>
+                </div>
             </v-col>
             <v-col class="v-col-12 v-col-lg-3">
                 <v-card class="pa-3 mb-3 elevation-2">
-                    <v-file-upload density="comfortable" variant="comfortable"></v-file-upload>
                     <v-select
                         v-model="course.must_complete_quizzes"
                         hide-details
@@ -131,6 +305,13 @@
                         variant="outlined"
                         density="comfortable"
                     />
+                    <v-file-upload
+                        density="comfortable"
+                        variant="comfortable"
+                        title="بارگذاری تصویر شاخص"
+                        class="mb-3"
+                    >
+                    </v-file-upload>
                     <v-btn block
                            size="large"
                            color="primary"
@@ -150,12 +331,14 @@
 <script setup>
 import {reactive, ref} from 'vue';
 import Editor from '@tinymce/tinymce-vue'
-import SeasonComponent from '../../../Components/SeasonComponent.vue';
-import draggable from 'vuedraggable';
 import AdminLayout from "../../../Layouts/AdminLayout.vue";
 import {router} from "@inertiajs/vue3";
 import {route} from "ziggy-js";
 import SearchableSelect from "@/Components/SearchableSelect.vue";
+import {VFileUpload} from 'vuetify/labs/VFileUpload'
+
+const switch_value = ref(true);
+const checkbox_value = ref(true);
 
 const props = defineProps({
     categories: Object,
@@ -220,3 +403,30 @@ function submitForm() {
     })
 }
 </script>
+<style scoped>
+.zo-close {
+    position: absolute;
+    top: -10px;
+    right: -10px
+}
+
+.zo-close button {
+    font-size: .75rem
+}
+
+.zo-actions {
+    position: absolute;
+    top: -5px;
+    left: -50px
+}
+
+.zo-actions .zo-switch {
+    transform: rotate(90deg);
+}
+
+.zo-actions .zo-switch .v-input {
+    display: flex;
+    justify-content: center;
+    align-items: center
+}
+</style>
