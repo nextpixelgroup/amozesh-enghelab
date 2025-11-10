fbeface3cfa196ebba21022554fbb3cb83dabaad
hosseinhezareh <hosseinhezareh2@gmail.com>
2025-11-10 20:15:32 +0330

    develop

--- database/migrations/2025_11_10_201130_add_avatar_id_to_users_table.php
+++ database/migrations/2025_11_10_201130_add_avatar_id_to_users_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-
             $table->foreignId('avatar_id')->nullable()->after('password')->constrained('media');
         });
     }
