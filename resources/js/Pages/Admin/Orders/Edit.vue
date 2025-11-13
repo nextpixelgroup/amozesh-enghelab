c5013d9797208e382106bda590890fe4eedb5990
stevenmaz <steven.m@realtyna.net>
2025-11-13 10:30:30 -0500

    Made some adjustments (UI)

--- resources/js/Pages/Admin/Orders/Edit.vue
+++ resources/js/Pages/Admin/Orders/Edit.vue
@@ -1,3 +1,14 @@
 <template>
-    order
+    <AdminLayout>
+        <Head title="Courses"/>
+    </AdminLayout>
 </template>
+<script>
+import {Head} from '@inertiajs/vue3'
+import {defineComponent} from "vue";
+import AdminLayout from "../../../Layouts/AdminLayout.vue";
+
+export default defineComponent({
+    components: {AdminLayout, Head}
+})
+</script>
