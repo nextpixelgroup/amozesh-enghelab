4a0d430700427b4f848862d9ab6db839f20f811c
stevenmaz <steven.m@realtyna.net>
2025-11-18 09:01:11 -0500

    Added The Course Details Page.

--- resources/js/Pages/Web/Courses/Show.vue
+++ resources/js/Pages/Web/Courses/Show.vue
@@ -1,6 +1,6 @@
 <template>
     <WebLayout>
-        <v-container class="zo-course-section">
+        <v-container class="zo-course-section" max-width="1260">
             <v-row dense>
                 <v-col cols="12" lg="9">
                     <v-card flat class="zo-card">
@@ -164,6 +164,55 @@
                                 </v-expansion-panel>
                             </v-expansion-panels>
                         </div>
+                        <div class="zo-prerequisites-courses-section">
+                            <div class="zo-courses-section">
+                                <strong class="zo-title">پیش نیازها</strong>
+                                <v-row dense>
+                                    <v-col cols="12" lg="6"
+                                           v-for="(course, index) in prerequisites"
+                                           :key="index"
+                                    >
+                                        <a href="#" class="zo-course">
+                                            <v-row dense>
+                                                <v-col cols="12" lg="6">
+                                                    <figure>
+                                                        <div class="zo-thumbnail">
+                                                            <img :src="course.image" class="img-fluid"/>
+                                                        </div>
+                                                        <div class="zo-category">
+                                                            <img src="/assets/img/site/c-cat.svg"
+                                                                 class="img-fluid"/>
+                                                            <span>{{ course.category }}</span>
+                                                        </div>
+                                                    </figure>
+                                                </v-col>
+                                                <v-col cols="12" lg="6">
+                                                    <div class="zo-content">
+                                                        <h2>{{ course.title }}</h2>
+                                                        <span class="zo-prof">{{ course.professor }}</span>
+                                                        <ul>
+                                                            <li>
+                                                                <img src="/assets/img/site/c-clock.svg"/>
+                                                                <span>{{ course.time }}</span>
+                                                            </li>
+                                                            <li>
+                                                                <img src="/assets/img/site/c-students.svg"/>
+                                                                <span>{{ course.students }} دانشجو</span>
+                                                            </li>
+                                                        </ul>
+                                                        <div class="zo-price">
+                                                            <span>قیمت</span>
+                                                            <strong>{{ course.price }}</strong>
+                                                        </div>
+                                                        <div class="zo-more">اطلاعات بیشتر</div>
+                                                    </div>
+                                                </v-col>
+                                            </v-row>
+                                        </a>
+                                    </v-col>
+                                </v-row>
+                            </div>
+                        </div>
                         <div class="zo-related-courses-section">
                             <div class="zo-courses-section">
                                 <strong class="zo-title">دوره‌های مرتبط</strong>
@@ -172,7 +221,7 @@
                                         dir="rtl"
                                         :slides-per-view="3.5"
                                         :space-between="35"
-                                        :modules="[Navigation, Pagination]"
+                                        :modules="[Navigation]"
                                         navigation
                                     >
                                         <swiper-slide
@@ -257,6 +306,13 @@
                         </div>
                     </v-card>
                 </v-col>
+                <v-col cols="12" lg="3">
+                    <v-card flat class="zo-card">
+                        <div class="zo-space">
+                            Sidebar
+                        </div>
+                    </v-card>
+                </v-col>
             </v-row>
         </v-container>
     </WebLayout>
@@ -320,6 +376,27 @@ const chapters = [
         ],
     },
 ];
+
+const prerequisites = ref([
+    {
+        image: '/assets/img/sample/1.png',
+        category: 'فلسفه',
+        title: 'دوره کتاب چهل حکمت نبوی (ص)',
+        professor: 'عباسی علیزاده',
+        time: '۰۳:۵۸',
+        students: '۲.۱۲۳',
+        price: 'رایگان',
+    },
+    {
+        image: '/assets/img/sample/2.png',
+        category: 'فلسفه',
+        title: 'دوره کتاب چهل حکمت نبوی (ص)',
+        professor: 'عباسی علیزاده',
+        time: '۰۳:۵۸',
+        students: '۲.۱۲۳',
+        price: 'رایگان',
+    },
+])
 
 const courses = ref([
     {
