7890cd308da4c67037e36498b351264aecc3cc8e
stevenmaz <steven.m@realtyna.net>
2025-11-23 04:28:39 -0500

    Added Index For Books

---
+++ resources/js/Pages/Web/Books/Index.vue
@@ -0,0 +1,319 @@
+<template>
+    <WebLayout>
+        <div class="zo-books-section">
+            <v-container>
+                <v-row dense>
+                    <v-col cols=12>
+                        <div class="zo-breadcrumbs-section">
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <a href="#">خانه</a>
+                                    </li>
+                                    <li>
+                                        <span>فروشگاه کتاب</span>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-container>
+            <v-container>
+                <v-row dense>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/16.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/17.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/18.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/19.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/20.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/21.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/22.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/23.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/24.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/19.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/16.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                    <v-col lg="3" md="6" cols="12">
+                        <a href="#" class="zo-book">
+                            <img src="assets/img/sample/18.png" alt="">
+                            <div class="zo-content">
+                                <h2>تربیت کودک</h2>
+                                <div class="zo-meta">
+                                    <div class="zo-star">
+                                        <i class="mdi mdi-star-outline"></i>
+                                        <span>۴.۹</span>
+                                    </div>
+                                    <div class="zo-price">
+                                        <strong>2.450.000</strong>
+                                        <small>تومان</small>
+                                    </div>
+                                </div>
+                                <div class="zo-add">
+                                    <i class="mdi mdi-cart-outline"></i>
+                                    <span>خرید آنلاین</span>
+                                </div>
+                            </div>
+                        </a>
+                    </v-col>
+                </v-row>
+            </v-container>
+            <v-container>
+                <v-row>
+                    <v-col cols="12">
+                        <v-pagination v-model="page" :length="8" rounded="circle"></v-pagination>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </div>
+    </WebLayout>
+</template>
+<script setup>
+import { ref } from 'vue'
+import WebLayout from "@/Layouts/WebLayout.vue";
+
+const sort = ref([
+    { title: 'جدیدترین' },
+    { title: 'قدیمی‌ترین' },
+])
+
+const category = ref([
+    'دسته‌بندی',
+    'فرهنگ و رسانه',
+    'انقلاب ایران',
+    'تفکر ذهن',
+])
+
+const model = ref('دسته‌بندی')
+const page = ref(1)
+
+</script>
