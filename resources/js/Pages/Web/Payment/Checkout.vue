15d269397ead42708a3ea70c0277f92344df61cd
stevenmaz <steven.m@realtyna.net>
2025-11-25 11:05:08 -0500

    Added The Checkout Page

--- resources/js/Pages/Web/Payment/Checkout.vue
+++ resources/js/Pages/Web/Payment/Checkout.vue
@@ -1,6 +1,6 @@
 <template>
     <WebLayout title="Checkout">
-        <div class="zo-cart-section">
+        <div class="zo-checkout-section">
             <v-container>
                 <v-row dense>
                     <v-col cols=12>
@@ -26,49 +26,146 @@
                 </v-row>
             </v-container>
             <v-container>
-                <v-row>
+                <v-row dense>
                     <v-col lg="8" cols="12">
                         <div class="zo-section">
                             <div class="zo-content">
                                 <h3>جزئیات پرداخت</h3>
-                                <v-row dense>
-                                    <v-col md="6" cols="12">
-                                        <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-account-circle" label="نام">
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col md="6" cols="12">
-                                        <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-account-circle" label="نام خانوادگی">
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col md="8" cols="12">
-                                        <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-map" label="آدرس کامل پستی">
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col md="4" cols="12">
-                                        <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-map-marker" label="کد پستی">
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col md="6" cols="12">
-                                        <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-cellphone" label="شماره موبایل">
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col md="6" cols="12">
-                                        <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-at" label="پست الکترونیک">
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col cols="12">
-                                        <v-textarea  hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-text" label="یادداشت‌هایی درباره سفارش شما، مثلاً نکات ویژه برای تحویل.">
-                                        </v-textarea>
-                                    </v-col>
-                                </v-row>
+                                <div class="zo-form">
+                                    <v-row dense>
+                                        <v-col md="6" cols="12">
+                                            <label>نام</label>
+                                            <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-account-circle" label="">
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col md="6" cols="12">
+                                            <label>نام خانوادگی</label>
+                                            <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-account-circle" label="">
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col md="8" cols="12">
+                                            <label>آدرس کامل پستی</label>
+                                            <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-map" label="">
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col md="4" cols="12">
+                                            <label>کد پستی</label>
+                                            <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-map-marker" label="">
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col md="6" cols="12">
+                                            <label>شماره موبایل</label>
+                                            <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-cellphone" label="">
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col md="6" cols="12">
+                                            <label>پست الکترونیک</label>
+                                            <v-text-field hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-at" label="">
+                                            </v-text-field>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <label>توضیحات</label>
+                                            <v-textarea hide-details density="compact" variant="outlined" prepend-inner-icon="mdi-text" label="یادداشت‌هایی درباره سفارش شما، مثلاً نکات ویژه برای تحویل.">
+                                            </v-textarea>
+                                        </v-col>
+                                    </v-row>
+                                </div>
+                            </div>
+                        </div>
+                    </v-col>
+                    <v-col lg="4" cols="12">
+                        <div class="zo-section">
+                            <div class="zo-content">
+                                <h3>سفارش شما</h3>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th class="text-right">کتاب</th>
+                                            <td class="text-left">جمع جزء</td>
+                                        </tr>
+                                    </thead>
+                                    <thead>
+                                        <tr>
+                                            <th>کتاب سفر شهادت گفتارهایی درباره واقعه عاشورا 1x</th>
+                                            <td class="text-left">
+                                                <div class="zo-price">
+                                                    <div class="zo-regular">
+                                                        <strong>250,000</strong>
+                                                        <small>تومان</small>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>کتاب یادمانده‌های سردشت 3x</th>
+                                            <td class="text-left">
+                                                <div class="zo-price">
+                                                    <div class="zo-regular">
+                                                        <strong>250,000</strong>
+                                                        <small>تومان</small>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>جمع جزء</th>
+                                            <td class="text-left">
+                                                <div class="zo-total">
+                                                    <div class="zo-price">
+                                                        <div class="zo-regular">
+                                                            <strong>755,350</strong>
+                                                            <small>تومان</small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                </table>
+                                <br>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>کتاب</th>
+                                            <td class="text-left">جمع جزء</td>
+                                        </tr>
+                                    </thead>
+                                    <thead>
+                                        <tr>
+                                            <th>حمل و نقل</th>
+                                            <td class="text-left">
+                                                <div class="zo-price">
+                                                    <div class="zo-regular">
+                                                        <strong>85,000</strong>
+                                                        <small>تومان</small>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>جمع کل</th>
+                                            <td class="text-left">
+                                                <div class="zo-total">
+                                                    <div class="zo-price">
+                                                        <div class="zo-regular">
+                                                            <strong>1,172,300</strong>
+                                                            <small>تومان</small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                </table>
+                                <div class="zo-bank">
+                                    <span>پرداخت امن زیبال</span>
+                                    <img src="/assets/img/sample/zibal.png" alt="">
+                                </div>
                             </div>
                         </div>
                         <div class="zo-actions">
-                            <v-btn variant="tonal" color="primary" class="zo-action">
-                                بروزرسانی سبد خرید
-                            </v-btn>
-                            <v-btn flat color="primary" class="zo-action">
-                                اقدام به پرداخت
+                            <v-btn block flat color="primary" class="zo-action">
+                                پرداخت آنلاین
                             </v-btn>
                         </div>
                     </v-col>
@@ -81,3 +178,9 @@
 import WebLayout from "@/Layouts/WebLayout.vue";
 
 </script>
+<style scoped>
+.table tr td {
+    border-bottom: 1px solid rgb(235, 235, 235);
+}
+
+</style>
