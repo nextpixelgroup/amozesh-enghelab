511c36f55d392ef13ce73ee15c38efa70911f40d
Ali Pixel <ali.mzi210@gmail.com>
2025-12-16 11:06:04 -0500

    Fix Breakpoints For Realted Courses

--- resources/js/Components/Web/Courses/CourseDetailsRelated.vue
+++ resources/js/Components/Web/Courses/CourseDetailsRelated.vue
@@ -1,45 +1,37 @@
 <template>
-    <div class="zo-courses-section" v-if="courses.length">
-        <div class="zo-swiper">
-            <swiper
-                dir="rtl"
-                :slides-per-view="3.5"
-                :space-between="10"
-                :modules="[Navigation]"
-                navigation
-                :breakpoints="breakpoints"
-            >
-                <swiper-slide v-for="(course, index) in courses" :key="index">
-                    <CourseCard :course="course" />
-                </swiper-slide>
-            </swiper>
-        </div>
+  <div class="zo-courses-section" v-if="courses.length">
+    <div class="zo-swiper">
+      <swiper
+        dir="rtl"
+        :modules="[Navigation]"
+        navigation
+        :breakpoints="breakpoints"
+      >
+        <swiper-slide v-for="(course, index) in courses" :key="index">
+          <CourseCard :course="course" />
+        </swiper-slide>
+      </swiper>
     </div>
+  </div>
 </template>
 
 <script setup>
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import CourseCard from "@/Components/Web/Courses/CourseCard.vue";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const props = defineProps({
-    courses: {
-        type: Array,
-    }
-})
+  courses: {
+    type: Array,
+    default: () => []
+  }
+});
 
 const breakpoints = {
-    320: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-    },
-    640: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-    },
-    1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 10,
-    }
-}
+  320: { slidesPerView: 1, spaceBetween: 10 },
+  640: { slidesPerView: 2, spaceBetween: 10 },
+  1024: { slidesPerView: 3.5, spaceBetween: 10 }
+};
 </script>
